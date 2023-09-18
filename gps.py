fe58dee81f91f565d29ee12075ef849dc867aa74
Nuxhi <78807626+Nuxhi@users.noreply.github.com>
2023-09-18 15:21:18 +0200

    Update gps.py

--- gps.py
+++ gps.py
@@ -1,41 +1,25 @@
-from geopy.geocoders import Nominatim
-from geopy.distance import great_circle
-import geocoder
+import requests
 
 def obtenir_coordonnees():
-    g = geocoder.ip('me')
-    return g.latlng
+    response = requests.get('https://ipinfo.io')
+    data = response.json()
+    lat, lon = data['loc'].split(',')
+    return float(lat), float(lon)
 
-def obtenir_ville_proche(lat, lon):
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    location = geolocator.reverse(f"{lat}, {lon}", exactly_one=True, language="fr")
-    current_location = (lat, lon)
-
-    # Obtenir la liste des emplacements dans les environs
-    locations = geolocator.reverse(f"{lat}, {lon}", language="fr", exactly_one=False)
-
-    # Initialiser les variables
-    ville_proche = None
-    distance_min = None
-
-    for loc in locations:
-        coords = (loc.latitude, loc.longitude)
-        distance = great_circle(current_location, coords).kilometers
-
-        if ville_proche is None or distance < distance_min:
-            distance_min = distance
-            ville_proche = loc.raw.get('address', {}).get('city', 'Non disponible')
-
-    return ville_proche
+def obtenir_ville(lat, lon):
+    response = requests.get(f'https://nominatim.openstreetmap.org/reverse?format=json&lat={lat}&lon={lon}&zoom=18')
+    data = response.json()
+    ville = data['address'].get('city', 'Non disponible')
+    return ville
 
 if __name__ == "__main__":
     # Obtention des coordonnées GPS
     latitude, longitude = obtenir_coordonnees()
 
-    # Obtention de la ville la plus proche
-    ville_proche = obtenir_ville_proche(latitude, longitude)
+    # Obtention de la ville
+    ville = obtenir_ville(latitude, longitude)
 
     # Affichage des résultats
     print(f"Latitude : {latitude}")
     print(f"Longitude : {longitude}")
-    print(f"Ville la plus proche : {ville_proche}")
+    print(f"Ville : {ville}")
