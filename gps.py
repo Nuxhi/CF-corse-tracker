f048d9b27763d11471cfa9f55fb43c0d43e35723
Nuxhi <78807626+Nuxhi@users.noreply.github.com>
2023-09-18 15:09:41 +0200

    Update gps.py

--- gps.py
+++ gps.py
@@ -1,49 +1,23 @@
-import ctypes
-
-def get_city_name():
-    try:
-        geolocation = ctypes.windll.winmm.timeGetTime()
-        latitude = (geolocation >> 32) * 1e-7
-        longitude = (geolocation & 0xffffffff) * 1e-7
-        return f"Latitude: {latitude}, Longitude: {longitude}"
-    except Exception as e:
-        return str(e)
-
-if __name__ == "__main__":
-    location_info = get_city_name()
-    print(f"Votre emplacement (latitude, longitude) est : {location_info}")
-
-
-
-import geocoder
-
-def get_city_name():
-    g = geocoder.ip('me')
-    return g.city
-
-if __name__ == "__main__":
-    city_name = get_city_name()
-    print(f"Vous êtes dans la ville de {city_name}")
-
-
----- 
 from geopy.geocoders import Nominatim
 import geocoder
 
-def get_city_name():
-    # Obtenir les coordonnées GPS
+def obtenir_coordonnees():
     g = geocoder.ip('me')
-    latitude, longitude = g.latlng
+    return g.latlng
 
-    # Utiliser les coordonnées pour obtenir la ville
+def obtenir_ville(lat, lon):
     geolocator = Nominatim(user_agent="geoapiExercises")
-    location = geolocator.reverse(f"{latitude}, {longitude}", exactly_one=True)
-
-    # Extraire le nom de la ville
-    address = location.raw.get('address', {})
-    city = address.get('city', '')
-    return city
+    location = geolocator.reverse(f"{lat}, {lon}", exactly_one=True, language="fr")
+    return location.raw['address']['city']
 
 if __name__ == "__main__":
-    city_name = get_city_name()
-    print(f"Vous êtes dans la ville de {city_name}")
+    # Obtention des coordonnées GPS
+    latitude, longitude = obtenir_coordonnees()
+
+    # Obtention de la ville
+    ville = obtenir_ville(latitude, longitude)
+
+    # Affichage des résultats
+    print(f"Latitude : {latitude}")
+    print(f"Longitude : {longitude}")
+    print(f"Ville : {ville}")
